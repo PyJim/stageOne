3c14fb58d2d32ca5b6d7b8636736a40b88adbb51
Jimmy <jimmykodwoessel@gmail.com>
2024-07-01 12:18:31 +0000

    allow all hosts

--- webserver/settings.py
+++ webserver/settings.py
@@ -30,9 +30,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e^51(=(eh++8#l9$h01l5vn2+f7ypa5v2w)ypzp4%(1=0t1+j%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
